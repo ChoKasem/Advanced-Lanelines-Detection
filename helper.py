b4d6d85af1dc6cb93fe13f601657591af42d5c23
Jo <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2020-05-24 21:55:57 -0400

    finish process_image

--- helper.py
+++ helper.py
@@ -3,6 +3,33 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 
+class Line():
+    n = 20
+    ploty = None
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False  
+        # x values of the last n fits of the line
+        self.recent_xfitted = [] 
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None     
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None  
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]  
+        #radius of curvature of the line in meter
+        self.radius_of_curvature = None 
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None 
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float') 
+        #x values for detected line pixels
+        self.allx = None  
+        #y values for detected line pixels
+        self.ally = None
+        self.pix_current_fit = None
+        self.pix_fitx = None
+        
 def calibration():
     #calibrate the camera and get distortion matrix
     
